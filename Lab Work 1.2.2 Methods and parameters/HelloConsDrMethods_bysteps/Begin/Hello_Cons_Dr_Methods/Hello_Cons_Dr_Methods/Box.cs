10d1e25fc6d81dfef3790706759ba85fc6ad90cc
TaranenkoVP <taranenkovp@gmail.com>
2016-02-21 17:13:04 +0200

    rectangle

---
+++ Lab Work 1.2.2 Methods and parameters/HelloConsDrMethods_bysteps/Begin/Hello_Cons_Dr_Methods/Hello_Cons_Dr_Methods/Box.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Cons_Dr_Methods
+{
+    class Box
+    {
+        public uint X { get; set; }
+        public uint Y { get; set; }
+        public uint Width { get; set; }
+        public uint Height { get; set; }
+        public string Message { get; set; }
+        public char Symbol { get; set; }
+
+        public void Draw()
+        {
+            if (Width > 0 && Height > 0)
+            {
+                if (Symbol == '*' || Symbol == '+' || Symbol == '$' || Symbol == '#' || Symbol == '@')
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                    if (Message == null)
+                    {
+                        Message = "";
+                    }
+
+                    string mes;
+
+                    if (Width >= 3 && Height >= 3)
+                    {
+                        mes = Message.Substring(0, Math.Min((int)Width - 2, Message.Length));
+                    }
+                    else
+                    {
+                        mes = "";
+                    }
+
+                    draw((int)X, (int)Y, (int)Width, (int)Height, ref mes, Symbol);
+                    Message = mes;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(@"""{0}"" - invalid symbol.", Symbol);
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("invalid Width or Height.");
+            }
+
+            Console.ResetColor();
+        }
+
+        private void draw(int X, int Y, int Width, int Height, ref string mes, char Symb)
+        {
+            // painting message...
+
+            if (Width >= 3 && Height >= 3)
+            {
+                Console.SetCursorPosition(X + (Width - mes.Length) / 2, Y + Height / 2);
+                Console.Write(mes);
+            }
+
+            // painting rectangle...
+
+            for (int i = 0; i < Width ; i++)
+            {
+                Console.SetCursorPosition(X + i, Y);
+                Console.Write(Symb);
+                Console.SetCursorPosition(X + i, Y + Height-1);
+                Console.Write(Symb);
+            }
+
+            for (int j = 1; j < Height-1 ; j++)
+            {
+                Console.SetCursorPosition(X, Y + j);
+                Console.Write(Symb);
+                Console.SetCursorPosition(X + Width-1, Y + j);
+                Console.Write(Symb);
+            }
+
+            Console.SetCursorPosition(X + Width+1 , Y + Height+1);
+            mes = "Square = " + Width * Height;
+        }
+    }
+}
---
+++ Lab Work 1.2.2 Methods and parameters/HelloConsDrMethods_bysteps/Begin/Hello_Cons_Dr_Methods/Hello_Cons_Dr_Methods/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Cons_Dr_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Box a = new Box();
+
+                Console.WriteLine("Type  position of the box, x = ");
+                a.X = uint.Parse(Console.ReadLine());
+
+                Console.WriteLine("Type  position of the box: y = ");
+                a.Y = uint.Parse(Console.ReadLine());
+
+                Console.WriteLine("Type  width of the box: width = ");
+                a.Width = uint.Parse(Console.ReadLine());
+
+                Console.WriteLine("Type  height of the box: height = ");
+                a.Height = uint.Parse(Console.ReadLine());
+
+                Console.WriteLine("Type symbol (*,+,$,#,@) :");
+                a.Symbol = char.Parse(Console.ReadLine());
+
+                Console.WriteLine("Type  the message : ");
+                a.Message = Console.ReadLine();
+
+                a.Draw();
+
+                Console.WriteLine();
+                Console.WriteLine(a.Message);
+
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("invalid parameters!");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
