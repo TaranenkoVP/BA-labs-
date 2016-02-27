73ff7be5d77106872381b87c6fccd5f61a5cdcf9
TaranenkoVP <taranenkovp@gmail.com>
2016-02-27 18:45:02 +0200

    Throwing an exception

---
+++ Lab Work 1.3.2 Throwing an exception/BirdFlewAwayException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usr_Exception
+{
+
+    #region Opr1
+
+    public class BirdFlewAwayException : ApplicationException
+    {
+        public DateTime When { get; set; }
+        public string Why { get; set; }
+
+        public BirdFlewAwayException() { }
+
+        public BirdFlewAwayException( string message, string cause, DateTime time )
+            : base(message)
+        {
+            Why = cause;
+            When = time;
+        }
+
+    }
+    #endregion
+}
