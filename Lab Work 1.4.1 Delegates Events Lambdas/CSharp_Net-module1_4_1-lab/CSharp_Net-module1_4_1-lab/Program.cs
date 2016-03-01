f9a2a7844b825eb9d39359a60ade118ef40c30e1
TaranenkoVP <taranenkovp@gmail.com>
2016-03-01 22:24:54 +0200

    Delegates Events Lambdas

---
+++ Lab Work 1.4.1 Delegates Events Lambdas/CSharp_Net-module1_4_1-lab/CSharp_Net-module1_4_1-lab/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_4_1_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 9) declare object of OnlineShop 
+            OnlineShop oshop = new OnlineShop();
+
+            // 10) declare several objects of Customer
+            Customer cust1 = new Customer("Vasiliy");
+            Customer cust2 = new Customer("Yasha");
+
+            // 11) subscribe method GotNewGoods() of every Customer instance 
+            // to event NewGoodsInfo of object of OnlineShop
+            oshop.evGodGoods += cust1.GotNewGoods;
+            oshop.evGodGoods += cust2.GotNewGoods;
+            oshop.evGodGoods += cust2.GotNewGoods;
+
+            // 12) invoke method NewGoods() of object of OnlineShop
+            // discuss results
+            oshop.NewGoods("hot pizza!!");
+            Console.ReadLine();
+        }
+    }
+}
